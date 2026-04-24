dbf1acaae524f9c0f60eb1b62f4db922121c266e
Martin Matovu <martin@matovius.dev>
2026-04-24 16:31:08 +0300

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '@fontsource-variable/radio-canada-big';
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	/**
+	 * @typedef Props
+	 * @prop { import('svelte').Snippet } children
+	 */
+
+	/** @type { Props } */
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
